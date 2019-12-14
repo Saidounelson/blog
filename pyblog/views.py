f8ea2d92494b4d8e082709f95921a43af91ddd4b
Saidou N <saidou.nassufou@wizall.com>
2019-12-14 20:36:19 +0000

    fix methode view for list post

--- pyblog/views.py
+++ pyblog/views.py
@@ -2,5 +2,15 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from pyblog.models import Post
 # Create your views here.
+
+def post_list(request):
+    posts = Post.objects.all()
+    
+    context = {
+        "post": posts
+    }
+    
+    return render(request,'pyblog/post_list.html',context)
+    
